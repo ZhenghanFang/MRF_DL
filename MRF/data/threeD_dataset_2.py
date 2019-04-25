3e5b5daa20ade119756fe5a66a47cb78bfca6dc7
Zhenghan Fang <45156169+ZhenghanFang@users.noreply.github.com>
2019-04-25 15:44:29 -0400

    Update threeD_dataset_2.py

--- MRF/data/threeD_dataset_2.py
+++ MRF/data/threeD_dataset_2.py
@@ -67,7 +67,7 @@ class MRFDataset(BaseDataset):
         person_path = ['190324_DLMRF3D_vol1','190324_DLMRF3D_vol2','190328_DLMRF3D_vol3','190330_DLMRF3D_vol4','190330_DLMRF3D_vol5','190407_DLMRF3D_vol6','190407_DLMRF3D_vol7']
         # slice_N = [94,94,94,94,94]
         # slice_N = [1,1,1,1,1]
-        slice_N = [142,142,142,142,142,142,142]
+        slice_N = [142,142,142,1,142,142,142]
         test_i = self.opt.test_i
         if self.opt.set_type == 'train':
             person = list(range(0,test_i))+list(range(test_i+1,5))
@@ -78,7 +78,7 @@ class MRFDataset(BaseDataset):
         for i in range(len(person)):
             for j in range(slice_N[person[i]]):
                 self.data_paths.append({
-                    'imMRF': d_root+person_path[person[i]]+'/2d/'+str(j+2)+'.mat',
-                    'Tmap': d_root+person_path[person[i]]+'/2d/'+str(j+2)+'.mat',
-                    'mask': d_root+person_path[person[i]]+'/2d/'+str(j+2)+'.mat'
+                    'imMRF': d_root+person_path[person[i]]+'/2d/'+str(j+200)+'.mat',
+                    'Tmap': d_root+person_path[person[i]]+'/2d/'+str(j+200)+'.mat',
+                    'mask': d_root+person_path[person[i]]+'/2d/'+str(j+200)+'.mat'
                     })
