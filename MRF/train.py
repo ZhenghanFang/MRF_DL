403efbbee6ee2febbe89a1bf18f55b14186e7f7f
Zhenghan Fang <45156169+ZhenghanFang@users.noreply.github.com>
2019-06-05 17:55:27 -0400

    Update train.py

--- MRF/train.py
+++ MRF/train.py
@@ -94,7 +94,7 @@ if host == 'stilson' or host == 'andrew' or host == 'wiggin' or host == 'enfield
     opt.checkpoints_dir = '/shenlab/lab_stor/zhenghan/checkpoints'
 elif host == 'badin' or host == 'bogue' or host == 'burgaw':
     opt.checkpoints_dir = '/shenlab/local/zhenghan/checkpoints'
-elif host == 'danbury' or host == 'denton' or host == 'elkin' or host == 'elkpark':
+elif host in ['danbury', 'denton', 'elkin', 'elkpark', 'dublin', 'dobson']:
     opt.checkpoints_dir = '/data/zhenghan/checkpoints'
 else:
     raise ValueError("cannot decide checkpoints_dir, server '%s' not recognized." % host)
