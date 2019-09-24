b69967277e877e760706089870db3e462d61d776
Zhenghan Fang <45156169+ZhenghanFang@users.noreply.github.com>
2019-09-23 17:37:21 -0700

    Update threeD_dataset_3.py

--- MRF/data/threeD_dataset_3.py
+++ MRF/data/threeD_dataset_3.py
@@ -175,4 +175,4 @@ class MRFDataset(BaseDataset):
             if not mask_file_name:
                 self.data3D[imMRF_path]['mask'] = self.data3D[imMRF_path]['t1'] * 0.0 + 1.0
             else:
-                self.data3D[imMRF_path]['mask'] = h5py.File(mask_path, 'r')['immask'][:]
+                self.data3D[imMRF_path]['mask'] = h5py.File(mask_path, 'r')['mask'][:]
